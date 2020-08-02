eb7d89e669dc2c8e6f6eb4e841d59285b327ac58
Pedro Augusto <pedroa@alu.ufc.br>
2020-08-02 14:49:49 -0300

    corrigir bug no form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -23,45 +23,50 @@ const Form = () => {
       </p>
 
       <div className='form__container'>
-        <input
-          className='form__username-input'
-          name='nome'
-          placeholder='Nome'
-          ref={register({ required: true, minLength: 2 })}
-        />
-        {errors.nome && errors.nome.type === 'required' && (
-          <p>Preencha o campo</p>
-        )}
-        {errors.nome && errors.nome.type === 'minLength' && (
-          <p>Escreva pelo menos 2 caracteres</p>
-        )}
-
-        <input
-          className='form__email-input'
-          name='email'
-          placeholder='Email'
-          ref={register({
-            required: 'Preencha o campo',
-            pattern: {
-              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-              message: 'Endereço de email invalido',
-            },
-          })}
-        />
-        {errors.email && errors.email.message && (
-          <p className='error'>{errors.email.message}</p>
-        )}
+        <div className='form__container__wrapper'>
+          <div className='form__container__wrapper__username'>
+            <input
+              className='form__container__wrapper__username__input'
+              name='nome'
+              placeholder='Nome'
+              ref={register({ required: true, minLength: 2 })}
+            />
+            {errors.nome && errors.nome.type === 'required' && (
+              <p className='error'>Preencha o campo</p>
+            )}
+            {errors.nome && errors.nome.type === 'minLength' && (
+              <p className='error'>Escreva pelo menos 2 caracteres</p>
+            )}
+          </div>
+          <div className='form__container__wrapper__email'>
+            <input
+              className='form__container__wrapper__email__input'
+              name='email'
+              placeholder='Email'
+              ref={register({
+                required: 'Preencha o campo',
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: 'Endereço de email invalido',
+                },
+              })}
+            />
+            {errors.email && errors.email.message && (
+              <p className='error'>{errors.email.message}</p>
+            )}
+          </div>
+        </div>
         <textarea
-          className='form__message-field'
+          className='form__container__message-field'
           placeholder='Mensagem'
           name='message'
           ref={register({ required: true, minLength: 15, maxLength: 60 })}
         />
         {errors.message && errors.message.type === 'minLength' && (
-          <p>Escreva pelo menos 15 caracteres</p>
+          <p className='error'>Escreva pelo menos 15 caracteres</p>
         )}
         {errors.message && errors.message.type === 'maxLength' && (
-          <p>Maximo de 60 caracteres</p>
+          <p className='error'>Maximo de 60 caracteres</p>
         )}
         <button className='form__send-button' type='submit'>
           enviar
